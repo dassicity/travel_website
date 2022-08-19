1d05224a0bd0fd6eef8808dec5139009d7f8f3de
Soumyanil Das <dassic@outlook.com>
2022-08-19 15:39:37 +0530

    Added handlebars files

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
 
 const app = express();
+
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main',
+}))
+
+app.set('views', './views');
+app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res, next) => {
-    res.type("text/plain");
-    res.send("TraVel WebSite");
+    // res.type("text/plain");
+    // res.send("TraVel WebSite");
+    res.render('home');
 });
 
 app.get('/about', (req, res, next) => {
-    res.type("text/plain");
-    res.send("About TraVel WebSite");
+    // res.type("text/plain");
+    // res.send("About TraVel WebSite");
+    res.render('about');
 });
 
 app.use((req, res, next) => {
