b9c4d79d2e16a2aba57d2f5b5be86d2429aa682b
Soumyanil Das <dassic@outlook.com>
2022-08-20 14:16:04 +0530

    Created handlers file for routes handling

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
+const fortune = require('./lib/fortune');
+const handlers = require('./lib/handlers');
 
 const app = express();
-
-const fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-]
 
 app.engine('handlebars', expressHandlebars.engine({
     defaultLayout: 'main',
@@ -22,31 +16,13 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res, next) => {
-    // res.type("text/plain");
-    // res.send("TraVel WebSite");
-    res.render('home');
-});
+app.get('/', handlers.home);
 
-app.get('/about', (req, res, next) => {
-    // res.type("text/plain");
-    // res.send("About TraVel WebSite");
-    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
-});
+app.get('/about', handlers.about);
 
-app.use((req, res, next) => {
-    // res.type('text/plain');
-    res.status(404);
-    res.render('404');
-});
+app.use(handlers.notFound);
 
-app.use((err, req, res, next) => {
-    console.error(err.message);
-    // res.type("text/plain");
-    res.status(500);
-    res.render('500')
-});
+app.use(handlers.serverError);
 
 app.listen(port, () => {
     console.log(`Express started on port ${port}. Press Ctrl-C to terminate.`)
